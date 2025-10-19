122c62fbe097025a5aadb0dbfc9f37d28359cdff
songjiashi <2944366318@qq.com>
2025-10-19 11:42:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 def count_characters():
-    # 从键盘输入一行字符
-    s = input()
-    
-    # 初始化计数器
-    letters = 0
-    digits = 0
-    spaces = 0
-    others = 0
-    
-    # 遍历字符串中的每个字符
-    for char in s:
-        if 'a' <= char <= 'z' or 'A' <= char <= 'Z':  # 英文字母
-            letters += 1
-        elif '0' <= char <= '9':  # 数字
-            digits += 1
-        elif char == ' ':  # 空格
-            spaces += 1
-        else:  # 其他字符
-            others += 1
-    
-    # 严格按照要求格式输出结果
-    print("英文字符: {}".format(letters))
-    print("数字: {}".format(digits))
-    print("空格: {}".format(spaces))
-    print("其他字符: {}".format(others))
+    try:
+        # 从键盘输入一行字符
+        s = input().rstrip('\n')  # 去除末尾的换行符
+        
+        # 初始化计数器
+        letters = 0
+        digits = 0
+        spaces = 0
+        others = 0
+        
+        # 遍历字符串中的每个字符
+        for char in s:
+            if char.isalpha():  # 英文字母
+                letters += 1
+            elif char.isdigit():  # 数字
+                digits += 1
+            elif char.isspace():  # 空格（包括制表符等）
+                spaces += 1
+            else:  # 其他字符
+                others += 1
+        
+        # 输出结果
+        print("英文字符: {}".format(letters))
+        print("数字: {}".format(digits))
+        print("空格: {}".format(spaces))
+        print("其他字符: {}".format(others))
+        
+    except Exception as e:
+        print("发生错误:", e)
 
-# 调用函数
+# 运行程序
 if __name__ == "__main__":
     count_characters()
